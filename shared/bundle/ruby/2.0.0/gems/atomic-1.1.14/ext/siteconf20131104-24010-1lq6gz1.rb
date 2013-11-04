8402b8bf5dae36c8c3b3f525d5bf59e1811b6772
zhupeijun <zhupeijun2006@163.com>
2013-11-04 16:19:18 +0900

    cap deploy:setup

---
+++ shared/bundle/ruby/2.0.0/gems/atomic-1.1.14/ext/siteconf20131104-24010-1lq6gz1.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20131104-24010-1ocoamt"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
